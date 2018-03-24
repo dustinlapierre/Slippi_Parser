759096daea41285289ba2d9ea790ab50464efe75
dustinlapierre <dustinlapierre@yahoo.com>
2018-03-24 19:20:53 -0400

    base working LSTM

--- LSTM2.py
+++ LSTM2.py
@@ -1,48 +1,41 @@
 from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.layers import Embedding
+from keras.layers import Dense, Embedding
 from keras.layers import LSTM
+from keras.datasets import imdb
 import numpy as np
 
-#define melee data set
-#using a fake for now
-#what do I do here????
-x = [[
-[20, -31, 9, -1],
-[20, -33, 9, -1],
-[20, -35, 9, -1],
-[20, -37, 9, -1],
-[20, -39, 9, -1],
-[20, -41, 9, -1],
-[20, -42, 9, -1],
-[18, -43, 9, -1],
-[20, -43, 9, 1],
-[20, -41, 9, 1]
-[20, -39, 9, 1],
-[20, -37, 9, 1],
-[20, -35, 9, 1],
-[20, -33, 9, 1],
-[20, -32, 9, 1],
-]]
+x = [
+    [[0],[1],[1],[0],[0],[0]],
+    [[0],[0],[1],[2],[2],[0]],
+    [[0],[0],[0],[0],[3],[3]],
+    [[0],[2],[2],[0],[0],[0]]
+]
+x = np.array(x,dtype=np.float32)
 
-#define model
-#16 melee input numbers so the input dim is 16xnumber of sequences?
+y_new = [
+    [[0],[1],[1],[1],[1],[1]],
+    [[0],[0],[1],[1],[0],[0]],
+    [[0],[0],[0],[0],[0],[0]],
+    [[0],[0],[0],[0],[0],[0]]
+]
+y_new = np.array(y_new, dtype=np.int32)
+
+
+print('Build model...')
 model = Sequential()
-#input shape number = number of inputs per time step
-#dense number = number of possible output actions
-model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2, input_shape=(None, 4)))
+model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2, input_shape=(None, 1), return_sequences=True))
 model.add(Dense(1, activation='sigmoid'))
 
-# Compile model
+# try using different optimizers and different optimizer configs
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-# Fit the model
 print('Train...')
-model.fit(x,y2,epochs=10)
+model.fit(x,y_new,epochs=300)
 pred = model.predict(x)
-predict_classes = np.argmax(pred,axis=1)
-print("Predicted classes: {}",predict_classes)
-print("Expected classes: {}",predict_classes)
+#predict_classes = np.argmax(pred,axis=2)
+print("Predicted classes:\n{}".format(pred > 0.5))
+#true_classes = np.argmax(y_new,axis=2)
+print("True classes:\n{}".format(y_new > 0.5))
