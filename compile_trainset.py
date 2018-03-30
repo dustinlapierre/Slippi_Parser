7c3b49ecc33582753495104402ef2b54998fee36
dustinlapierre <dustinlapierre@yahoo.com>
2018-03-30 11:51:54 -0400

    cleanup

--- compile_trainset.py
+++ compile_trainset.py
@@ -7,10 +7,7 @@ def compile_data():
     data = []
     path = 'training_set/data'
     for filename in os.listdir(path):
-        #firstNlines=myfile.readlines()[0:5]
-        #head = [next(myfile) for x in xrange(N)]
         file = open(('training_set/data/' + filename), "r")
-        #mylist = ast.literal_eval(list(islice(file, 100).strip('\n')))
         mylist = ast.literal_eval(file.read())
         data.append(mylist)
         file.close()
@@ -22,10 +19,7 @@ def compile_labels():
     labels = []
     path = 'training_set/labels'
     for filename in os.listdir(path):
-        #firstNlines=myfile.readlines()[0:5]
-        #head = [next(myfile) for x in xrange(N)]
         file = open(('training_set/labels/' + filename), "r")
-        #mylist = ast.literal_eval(list(islice(file, 100).strip('\n')))
         mylist = ast.literal_eval(file.read())
         labels.append(mylist)
         file.close()
