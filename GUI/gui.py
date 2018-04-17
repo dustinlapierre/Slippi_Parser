f39799e7df0e444374bc8f4f6f136f2880d5741e
dustinlapierre <dustinlapierre@yahoo.com>
2018-04-17 00:43:00 -0400

    bare bones working gui

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,44 +1,46 @@
-from kivy import properties
+import kivy
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.graphics import Canvas, Color, Rectangle
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.image import Image
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.properties import StringProperty
 
-player1_character = ""
-player2_character = ""
-stage = ""
+player1_character = "None"
+player2_character = "None"
+stage = "None"
 shared_queue = None
 player1_stocks = 0
 player2_stocks = 0
 commentary = "" #dont worry about this yet
 
-
-class MainScreen(FloatLayout):
-    content = properties.StringProperty()
-    lines = 0
-    contentList = []
-    outputString = ""
-    lastLine = ""
+class MainView(FloatLayout):
+    stage_label = StringProperty()
+    char1_label = StringProperty()
+    char2_label = StringProperty()
+    p1_stocks = StringProperty()
+    p2_stocks = StringProperty()
 
     def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-        self.label = self.ids.lbl_console_window
+        super(FloatLayout, self).__init__(**kwargs)
+        self.stage_label = stage
+        self.char1_label = player1_character
+        self.char2_label = player2_character
 
     def update(self, *args):
+        global player1_stocks
+        global player2_stocks
+
         if(not shared_queue.empty()):
             stock_update = shared_queue.get()
             player1_stocks = stock_update[0]
             player2_stocks = stock_update[1]
-            print(player1_stocks, player2_stocks)
             shared_queue.task_done()
+        self.p1_stocks = str(player1_stocks)
+        self.p2_stocks = str(player2_stocks)
 
-class MainApp(App):
+class GuiApp(App):
     def build(self):
-        main = MainScreen()
-        Clock.schedule_interval(main.update, 0.1)
+        main = MainView()
+        Clock.schedule_interval(main.update, 0.05)
         return main
 
 def GuiThreadStart(character1, character2, current_stage, connection):
@@ -50,4 +52,4 @@ def GuiThreadStart(character1, character2, current_stage, connection):
     stage = current_stage
     global shared_queue
     shared_queue = connection
-    MainApp().run()
+    GuiApp().run()
