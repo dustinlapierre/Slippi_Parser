205e622efb609c0d94ead127b97c189ef6707bcc
dustinlapierre <dustinlapierre@yahoo.com>
2018-04-18 23:22:36 -0400

    extract method on file watcher

--- memory_parser.py
+++ memory_parser.py
@@ -1,6 +1,3 @@
-import os
-import win32file
-import win32con
 import threading
 from queue import Queue
 import time
@@ -11,74 +8,15 @@ import structures
 from analytics import *
 from commentary_statements import *
 from parse_functions import *
+from file_detection import watch_for_create
 
 import sys
 sys.path.insert(0, 'GUI/')
 from gui import GuiThreadStart
 
-#Windows filesystem watcher code written by Tim Golden
-#Parser written by Dustin Lapierre
+#Written by Dustin Lapierre copyright 04/18/2018
 
-ACTIONS = {
-  1 : "Created",
-  2 : "Deleted",
-  3 : "Updated",
-  4 : "Renamed from something",
-  5 : "Renamed to something"
-}
-# Thanks to Claudio Grondi for the correct set of numbers
-FILE_LIST_DIRECTORY = 0x0001
-
-path_to_watch = "."
-hDir = win32file.CreateFile (
-  path_to_watch,
-  FILE_LIST_DIRECTORY,
-  win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE | win32con.FILE_SHARE_DELETE,
-  None,
-  win32con.OPEN_EXISTING,
-  win32con.FILE_FLAG_BACKUP_SEMANTICS,
-  None
-)
-full_filename = ""
-while 1:
-    #
-    # ReadDirectoryChangesW takes a previously-created
-    # handle to a directory, a buffer size for results,
-    # a flag to indicate whether to watch subtrees and
-    # a filter of what changes to notify.
-    #
-    # NB Tim Juchcinski reports that he needed to up
-    # the buffer size to be sure of picking up all
-    # events when a large number of files were
-    # deleted at once.
-    #
-    results = win32file.ReadDirectoryChangesW (
-        hDir,
-        1024,
-        True,
-        win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
-         win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
-         win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
-         win32con.FILE_NOTIFY_CHANGE_SIZE |
-         win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
-         win32con.FILE_NOTIFY_CHANGE_SECURITY,
-        None,
-        None
-      )
-
-    #watch for file creations and updates within the current folder
-    #when the Slippi file updates, read the changes
-    #parse melee data from changes
-    #update display with data from newest frame
-    for action, file in results:
-        full_filename = os.path.join (path_to_watch, file)
-        #watch for file creation flag then save fd
-        if(ACTIONS.get(action, "Unknown") == "Created"):
-            break;
-        else:
-            full_filename = ""
-    if(full_filename != ""):
-        break;
+full_filename = watch_for_create(".")
 
 def post_frame_as_list():
     data = []
@@ -192,7 +130,7 @@ with open(full_filename, "rb") as replay:
     #game start
     data = read_frame(replay, 320)
     parse_game_start(data)
-    
+
     player1_character = translator.external_character_id[game_start_data.character_ID_port1]
     player2_character = translator.external_character_id[game_start_data.character_ID_port2]
 
